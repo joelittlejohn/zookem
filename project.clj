a91d343e1f2bcd42ebee1285a12efdf714622fd5
Joe Littlejohn <joe.littlejohn@nokia.com>
2014-08-07 17:19:46 +0100

    lein-release plugin: preparing 0.1.2 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject zookem "0.1.2-SNAPSHOT"
+(defproject zookem "0.1.2"
   :description "A helper for running embedded Zookeeper instances for integration testing."
   :url "https://github.com/joelittlejohn/zookem"
   :license {:name "Eclipse Public License"
